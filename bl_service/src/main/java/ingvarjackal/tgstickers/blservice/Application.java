a23f1c85bf3deb8d9f813f5fea15658fad310814
IngvarJackal <rz703a@gmail.com>
2017-04-20 12:48:17 +0300

    +connectivity

--- bl_service/src/main/java/ingvarjackal/tgstickers/blservice/Application.java
+++ bl_service/src/main/java/ingvarjackal/tgstickers/blservice/Application.java
@@ -5,16 +5,35 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 @SpringBootApplication
 @RestController
 public class Application {
     @RequestMapping("/")
-    public String home() {
-        return "it's BlService application!";
+    public String home() throws Exception {
+        return "it's BlService application which requested outService: " + getHTML(System.getenv("OUTSERVICE_PATH"));
     }
 
     public static void main(String[] args) {
         SpringApplication.run(ingvarjackal.tgstickers.blservice.Application.class, args);
+    }
+
+    public static String getHTML(String urlToRead) throws Exception {
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(urlToRead);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        rd.close();
+        return result.toString();
     }
 }
 
--- in_service/src/main/java/ingvarjackal/tgstickers/inservice/Application.java
+++ in_service/src/main/java/ingvarjackal/tgstickers/inservice/Application.java
@@ -5,16 +5,36 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 @SpringBootApplication
 @RestController
 public class Application {
     @RequestMapping("/")
-    public String home() {
-        return "it's InService application!";
+    public String home() throws Exception {
+        return "it's InService application which requested outService: " + getHTML(System.getenv("BLSERVICE_PATH"));
     }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    public static String getHTML(String urlToRead) throws Exception {
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(urlToRead);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        rd.close();
+        return result.toString();
+    }
+
 }
 
