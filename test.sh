4cdc298c93089b84a8d68a85d5f07b3f7f1fe8c3
IngvarJackal <rz703a@gmail.com>
2017-05-24 12:25:11 +0300

    moved tests to proper dir

---
+++ test.sh
@@ -0,0 +1,19 @@
+export BOT_TOKEN=testtoken
+cd commons &&
+mvn clean install &&
+cd .. &&
+cd bl_service &&
+mvn clean test &&
+rm -r src/main/docker/target/ &&
+cp -r target/ src/main/docker/ &&
+cd .. &&
+cd in_service &&
+mvn clean test &&
+rm -r src/main/docker/target/ &&
+cp -r target/ src/main/docker/ &&
+cd .. &&
+cd out_service &&
+mvn clean test &&
+rm -r src/main/docker/target/ &&
+cp -r target/ src/main/docker/ &&
+cd ..
