b3279421eea170f887de3002b742ad5060b15e71
IngvarJackal <rz703a@gmail.com>
2017-05-24 12:54:46 +0300

    fix travis3

--- test.sh
+++ test.sh
@@ -4,13 +4,9 @@ mvn clean install &&
 cd .. &&
 cd bl_service &&
 mvn clean test &&
-rm -r src/main/docker/target/ &&
-cp -r target/ src/main/docker/ &&
 cd .. &&
 cd in_service &&
 mvn clean test &&
-rm -r src/main/docker/target/ &&
-cp -r target/ src/main/docker/ &&
 cd .. &&
 cd out_service &&
 mvn clean test
