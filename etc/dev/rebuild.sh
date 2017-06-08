fd22e3afafb2c247e30e69ce686d7465bd1f370a
IngvarJackal <rz703a@gmail.com>
2017-06-08 11:28:53 +0300

    fixed &&

--- etc/dev/rebuild.sh
+++ etc/dev/rebuild.sh
@@ -10,17 +10,17 @@ mvn clean install &&
 cd .. &&
 cd bl_service &&
 mvn clean package &&
-rm -r src/main/docker/target/ &&
+rm -r src/main/docker/target/
 cp -r target/ src/main/docker/ &&
 cd .. &&
 cd in_service &&
 mvn clean package &&
-rm -r src/main/docker/target/ &&
+rm -r src/main/docker/target/
 cp -r target/ src/main/docker/ &&
 cd .. &&
 cd out_service &&
 mvn clean package &&
-rm -r src/main/docker/target/ &&
+rm -r src/main/docker/target/
 cp -r target/ src/main/docker/ &&
 cd .. &&
 docker rmi -f tgstickers_inservice
