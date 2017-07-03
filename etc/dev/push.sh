253787ab8d5aa369f85c7e263570d187a290f5dc
IngvarJackal <rz703a@gmail.com>
2017-07-03 11:12:47 +0300

    optimized build a but

--- etc/dev/push.sh
+++ etc/dev/push.sh
@@ -1,7 +1,6 @@
-cd commons && mvn clean install && cd ..
-cd bl_service && mvn clean package && mvn docker:build && cd ..
-cd in_service && mvn clean package && mvn docker:build && cd ..
-cd out_service && mvn clean package && mvn docker:build && cd ..
-docker push ingvarjackal/tgstickers-blservice
-docker push ingvarjackal/tgstickers-inservice
+docker tag tgstickers_inservice ingvarjackal/tgstickers-inservice &&
+docker tag tgstickers_blservice ingvarjackal/tgstickers-blservice &&
+docker tag tgstickers_outservice ingvarjackal/tgstickers-outservice &&
+docker push ingvarjackal/tgstickers-blservice &&
+docker push ingvarjackal/tgstickers-inservice &&
 docker push ingvarjackal/tgstickers-outservice
