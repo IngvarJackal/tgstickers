28979b6c1ab62ed1a784ff904f9a642f15b3fb30
IngvarJackal <rz703a@gmail.com>
2017-07-13 14:32:11 +0300

    fixed script

--- etc/prod/restart.sh
+++ etc/prod/restart.sh
@@ -17,9 +17,9 @@ kill `ps -aux | grep inservice_keeper | grep -v grep | awk '{ print $2 }'` 2> /d
 kill `ps -aux | grep blservice_keeper | grep -v grep | awk '{ print $2 }'` 2> /dev/null
 kill `ps -aux | grep outservice_keeper | grep -v grep | awk '{ print $2 }'` 2> /dev/null
 nohup ./docker-compose -f docker-compose-prod.yml up > logs 2>&1 < /dev/null &
-echo 'while true; do sleep 60; if [[ "OK" == $(curl localhost:10000 2> /dev/null) ]] ; then echo $(date) "OK"; else echo $(date) "NOK"; ./docker-compose -f docker-compose-prod.yml restart inservice; done' > inservice_keeper.sh; fi'
-echo 'while true; do sleep 60; if [[ "OK" == $(curl localhost:10001 2> /dev/null) ]] ; then echo $(date) "OK"; else echo $(date) "NOK"; ./docker-compose -f docker-compose-prod.yml restart blservice; done' > blservice_keeper.sh; fi'
-echo 'while true; do sleep 60; if [[ "OK" == $(curl localhost:10002 2> /dev/null) ]] ; then echo $(date) "OK"; else echo $(date) "NOK"; ./docker-compose -f docker-compose-prod.yml restart outservice; done' > outservice_keeper.sh; fi'
+echo 'while true; do sleep 60; if [[ "OK" == $(curl localhost:10000 2> /dev/null) ]] ; then echo $(date) "OK"; else echo $(date) "NOK"; ./docker-compose -f docker-compose-prod.yml restart inservice; done'; fi > inservice_keeper.sh
+echo 'while true; do sleep 60; if [[ "OK" == $(curl localhost:10001 2> /dev/null) ]] ; then echo $(date) "OK"; else echo $(date) "NOK"; ./docker-compose -f docker-compose-prod.yml restart blservice; done'; fi > blservice_keeper.sh
+echo 'while true; do sleep 60; if [[ "OK" == $(curl localhost:10002 2> /dev/null) ]] ; then echo $(date) "OK"; else echo $(date) "NOK"; ./docker-compose -f docker-compose-prod.yml restart outservice; done'; fi > outservice_keeper.sh
 nohup sh inservice_keeper.sh > inservice_keeper_logs 2>&1 < /dev/null &
 nohup sh blservice_keeper.sh > blservice_keeper_logs 2>&1 < /dev/null &
 nohup sh outservice_keeper.sh > outservice_keeper_logs 2>&1 < /dev/null &
