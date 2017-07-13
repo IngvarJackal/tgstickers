5dd9aa2aaee4f5f9cf2b5ec6ab7c9383eb635528
IngvarJackal <rz703a@gmail.com>
2017-07-13 12:31:36 +0300

    +robustness?

--- etc/prod/restart.sh
+++ etc/prod/restart.sh
@@ -13,4 +13,13 @@ else
 fi
 wget -q https://raw.githubusercontent.com/IngvarJackal/tgstickers/master/etc/prod/docker-compose-prod.yml -O docker-compose-prod.yml
 ./docker-compose -f docker-compose-prod.yml pull
+kill `ps -aux | grep inservice_keeper | grep -v grep | awk '{ print $2 }'` 2> /dev/null
+kill `ps -aux | grep blservice_keeper | grep -v grep | awk '{ print $2 }'` 2> /dev/null
+kill `ps -aux | grep outservice_keeper | grep -v grep | awk '{ print $2 }'` 2> /dev/null
 nohup ./docker-compose -f docker-compose-prod.yml up > logs 2>&1 < /dev/null &
+echo 'while true; do sleep 60; [[ "OK" == $(curl localhost:10000 2> /dev/null) ]] && echo $(date) "OK" || echo $(date) "NOK"; ./docker-compose -f docker-compose-prod.yml restart inservice; done' > inservice_keeper.sh
+echo 'while true; do sleep 60; [[ "OK" == $(curl localhost:10001 2> /dev/null) ]] && echo $(date) "OK" || echo $(date) "NOK"; ./docker-compose -f docker-compose-prod.yml restart blservice; done' > blservice_keeper.sh
+echo 'while true; do sleep 60; [[ "OK" == $(curl localhost:10002 2> /dev/null) ]] && echo $(date) "OK" || echo $(date) "NOK"; ./docker-compose -f docker-compose-prod.yml restart outservice; done' > outservice_keeper.sh
+nohup sh inservice_keeper.sh > inservice_keeper_logs 2>&1 < /dev/null &
+nohup sh blservice_keeper.sh > blservice_keeper_logs 2>&1 < /dev/null &
+nohup sh outservice_keeper.sh > outservice_keeper_logs 2>&1 < /dev/null &
